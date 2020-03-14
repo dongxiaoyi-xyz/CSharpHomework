673ddfbf48fc33b5b8ed4fd0f651cde63f373f1c
dongxiaoyi-xyz <61344296+dongxiaoyi-xyz@users.noreply.github.com>
2020-03-14 20:32:33 +0800

    Add files via upload

---
+++ Homework4/AlarmClock/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlarmClock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Form form1 = new Form();
+            form1.alarmClock1.Tick();//模拟闹钟滴答
+            form1.alarmClock1.Alarm();//模拟闹钟响铃
+        }
+
+    }
+
+    public delegate void AlmClkHandler(object sender);
+
+    class AlarmClock
+    {
+        //定义滴答（OnTick）事件和响铃（OnAlarm）事件
+        public event AlmClkHandler OnTick;
+        public event AlmClkHandler OnAlarm;
+
+        public void Tick()
+        {
+            //触发OnTick事件
+            OnTick(this);
+        }
+
+        public void Alarm()
+        {
+            //触发OnAlarm事件
+            OnAlarm(this);
+        }
+    }
+
+    class Form
+    {
+        public AlarmClock alarmClock1;
+        public Form()
+        {
+            alarmClock1 = new AlarmClock();
+            alarmClock1.OnTick += PrintTick;
+            alarmClock1.OnAlarm += PrintAlarm;
+        }
+
+        //控制台输出滴答事件信息
+        void PrintTick(object sender)
+        {
+            Console.WriteLine("滴答");
+        }
+
+        //控制台输出响铃事件信息
+        void PrintAlarm(object sender)
+        {
+            Console.WriteLine("叮铃铃，叮铃铃……");
+        }
+    }
+}
---
+++ Homework4/ListForEach/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListForEach
+{
+    class Program
+    {
+
+        delegate void Func(string n);
+
+        static void Main(string[] args)
+        {
+            List<int> intList = new List<int>();
+            Random rd = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                intList.Add(rd.Next(100));
+            }
+            int min = intList[1];
+            int max = intList[1];
+            int sum = 0;
+            Console.WriteLine("随机生成的10个链表元素为");
+            intList.ForEach(s =>  Console.WriteLine(s));
+            intList.ForEach(e=> { if (e < min) min = e; });
+            intList.ForEach(e => { if (e > max) max = e; });
+            intList.ForEach(e => sum += e);
+            Console.WriteLine($"链表元素的最大值为{max}");
+            Console.WriteLine($"链表元素的最小值为{min}");
+            Console.WriteLine($"链表元素的和为{sum}");
+        }
+    }
+}
