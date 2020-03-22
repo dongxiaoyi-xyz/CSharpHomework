8d1666501d49a103d7d30a58a00e090fa89266ca
dongxiaoyi-xyz <61344296+dongxiaoyi-xyz@users.noreply.github.com>
2020-03-22 14:34:32 +0800

    Add files via upload

---
+++ Homework5/OrderManage/Customer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManage
+{
+    class Customer
+    {
+        public int id;                      //用户账号
+        public string name;                 //用户名
+
+        public Customer(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+
+        //重写ToString方法
+        public override string ToString()
+        {
+            return "Customer:"+"\tid--"+id+"\tname--"+name;
+        }
+    }
+}
